6cfdfff44263c3c432274ab033d9e926b1b87690
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-02 20:12:32 +0100

    log specific things

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -6,9 +6,13 @@ export default {
         setTimeout(() => {
             this.onUnload = Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
                 const event = args[0]
-                console.log(args)
-                if (event.type === "MESSAGE_DELETE") {
-                    Vendetta.ui.toasts.showToast("hello")
+                const type: string = event.type
+                if (type === "MESSAGE_CREATE") {
+                    console.log(args)
+                } else if (type === "MESSAGE_DELETE") {
+                    console.log(args)
+                } else if (type.startsWith("MESSAGE")) {
+                    console.log(args)
                 }
             })
         }, 10000)
