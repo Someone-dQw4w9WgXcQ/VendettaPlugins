c2a1625d81107683b93dd053f859ec217a83721f
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-02 18:03:00 +0100

    toasts instead of log

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -4,7 +4,7 @@ import Settings from "./Settings";
 export default {
     onLoad: () => {
         Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
-            Vendetta.logger.log(args)
+            Vendetta.ui.toasts.showToast(args.toString())
         })
     },
     onUnload: () => {
