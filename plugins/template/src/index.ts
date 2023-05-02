704f3b0d0b4666d8128eb8ffe03959fe970975d4
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-02 20:31:50 +0100

    save in storage & log times

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,18 +1,21 @@
 import Vendetta from "@vendetta";
 import Settings from "./Settings";
 
+const data = Vendetta.plugin.storage
+
 export default {
     onLoad() {
         setTimeout(() => {
             this.onUnload = Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
                 const event = args[0]
                 const type: string = event.type
+                // would not be surprised if this uses a lot of memory...
                 if (type === "MESSAGE_CREATE") {
-                    console.log(args)
+                    data[event.message.id] = event.message
                 } else if (type === "MESSAGE_DELETE") {
-                    console.log(args)
-                } else if (type.startsWith("MESSAGE")) {
-                    console.log(args)
+                    console.log(data[event.message.id], "deleted at", Vendetta.metro.common.moment().format("hh:mm:ss.SS"))
+                } else if (type == "MESSAGE_UPDATE") {
+                    console.log(data[event.message.id], "edited at", Vendetta.metro.common.moment().format("hh:mm:ss.SS"), "to", event.message)
                 }
             })
         }, 10000)
