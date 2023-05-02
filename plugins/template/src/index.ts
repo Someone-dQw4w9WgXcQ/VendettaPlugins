cd865c4b1723f2e6df1e5235ab03a3d0d132a70e
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-02 18:21:27 +0100

    stop crashing please

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -2,17 +2,14 @@ import Vendetta from "@vendetta";
 import Settings from "./Settings";
 
 export default {
-    onLoad: () => {
+    onLoad() {
         setTimeout(() => {
-            Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
+            this.onUnload = Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
                 if (args[0].type === "MESSAGE_DELETE") {
                     Vendetta.ui.toasts.showToast(args[0].toString())
                 }
             })
         }, 10000)
-    },
-    onUnload: () => {
-
     },
     settings: Settings,
 }
