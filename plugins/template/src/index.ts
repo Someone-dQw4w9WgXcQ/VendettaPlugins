a70a1754ee642a1629554ac5d37957f3da11dec5
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-02 20:41:12 +0100

    better logs

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -11,11 +11,12 @@ export default {
                 const type: string = event.type
                 // would not be surprised if this uses a lot of memory...
                 if (type === "MESSAGE_CREATE") {
+                    console.log("create", event.message)
                     data[event.message.id] = event.message
                 } else if (type === "MESSAGE_DELETE") {
                     console.log(data[event.message.id], "deleted at", Vendetta.metro.common.moment().format("hh:mm:ss.SS"))
                 } else if (type == "MESSAGE_UPDATE") {
-                    console.log(data[event.message.id], "edited at", Vendetta.metro.common.moment().format("hh:mm:ss.SS"), "to", event.message)
+                    console.log(data[event.message.id], "edited at", event.message.edited_timestamp, "to", event.message.content)
                 }
             })
         }, 10000)
