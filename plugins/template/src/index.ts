5e4265f126138124117f58dc5474013b87a4bf14
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-02 18:09:56 +0100

    delay

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -3,12 +3,14 @@ import Settings from "./Settings";
 
 export default {
     onLoad: () => {
-        Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
-            Vendetta.ui.toasts.showToast(args.toString())
-        })
+        setTimeout(() => {
+            Vendetta.patcher.before("dispatch", Vendetta.metro.common.FluxDispatcher, (args) => {
+                Vendetta.ui.toasts.showToast(args.toString())
+            })
+        }, 10000)
     },
     onUnload: () => {
-        
+
     },
     settings: Settings,
 }
