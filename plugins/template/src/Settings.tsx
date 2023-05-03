6a1d8ac6f8f722469f377188b26ece52b8f8798c
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-03 16:41:55 +0100

    why does it crash

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -3,12 +3,14 @@ import { Forms } from "@vendetta/ui/components";
 const { FormText } = Forms;
 const storage = Vendetta.plugin.storage
 
+console.log("a")
 let timeInstalled
 if ("timeInstalled" in storage) {
     timeInstalled = storage.timeInstalled
 } else {
     timeInstalled = storage.timeInstalled = Vendetta.metro.common.moment()
 }
+console.log(timeInstalled)
 
 export default () => (
     <FormText>
