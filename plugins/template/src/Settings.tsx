f913f4a6bae6a67f88de433298400167fb624cc5
Someone-dQw4w9WgXcQ <dcheunggb@outlook.com>
2023-05-03 16:57:15 +0100

    better formatting & find events

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -8,7 +8,7 @@ let timeInstalled
 if ("timeInstalled" in storage) {
     timeInstalled = storage.timeInstalled
 } else {
-    timeInstalled = storage.timeInstalled = Vendetta.metro.common.moment().format("hh:mm:ss.SS")
+    timeInstalled = storage.timeInstalled = Vendetta.metro.common.moment().format("DD/MM/YYYY HH:mm.ss")
 }
 
 export default () => (
--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -26,11 +26,13 @@ export default {
                 } else if (type == "MESSAGE_UPDATE") {
                     const saved = data[event.message.id]
                     if (saved) {
-                        event.message.content = `${saved.content}\n${event.message.content}(edited${event.message.edited_timestamp})`
+                        event.message.content = `\`${saved.content}\n${event.message.content} (edited ${event.message.edited_timestamp})\``
                     } else {
                         console.log("edited at", event.message.edited_timestamp, "to", event.message)
                         event.message.content += ` (edited${event.message.edited_timestamp})`
                     }
+                } else if (type.startsWith("MESSAGE")) {
+                    console.log(type)
                 }
                 return args
             })
